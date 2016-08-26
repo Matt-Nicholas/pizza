6112a22c66687c93ebcaf4d357beb34b95432de5
Matt-Nicholas <fostermatt82@gmail.com>
2016-08-25 22:49:40 -0700

    Add function to calculate the cost of multiple pizzas and their toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,48 @@
-function PizzaMaker(size, style, flavoredCrust){
+function PizzaMaker(size, style, toppings, flavoredCrust){
     this.pSize = size;
     this.pStyle = style;
-    this.toppings = ['pep', 'sausaage', 'onion', 'anchovie'];
+    this.toppings = [];
     this.flavoredCrust = flavoredCrust;
 }
-
-
-
-PizzaMaker.prototype.toppingsCharge = function(){
+// Topping Cost Calculator
+PizzaMaker.prototype.toppingsCharge = function(j){
     var toppingsCharge = 0;
     for (var i = 0; i < pizza.toppings.length; i++) {
         toppingsCharge += 0.5;
     }
-    if(pizza.pSize === 'md'){
+    if(pizzasOrdered[j].pSize === 'md'){
         toppingsCharge *= 1.5;
-    }else if (pizza.pSize === 'lg') {
+    }else if (pizzasOrdered[j].pSize === 'lg') {
         toppingsCharge *= 2;
     }
     return toppingsCharge;
 }
-var selectedToppings = ['sausage', 'pepperoni', 'onion', 'greenPepper'];
-var toppins = [];
 
+var selectedToppings = ['sausage', 'pepperoni', 'onion', 'greenPepper'];
 function addToppings(){
-for (var i = 0; i < selectedToppings.length; i++) {
-    pizza.toppings.push(selectedToppings[i]);
+    for (var i = 0; i < selectedToppings.length; i++) {
+        pizza.toppings.push(selectedToppings[i]);
+    }
 }
+var pizzasOrdered = [];
+// function addPizza(){
+//     var pizza = new PizzaMaker('lg', 'deep', 'garlic');
+//     return pizza;
+// }
+function addPizzas(pizza){
+    pizzasOrdered.push(pizza);
 }
+
+// Total Cost Calculator
+function totalCostCalculator(){
+    var charges = 0;
+    for (var i = 0; i < pizzasOrdered.length; i++) {
+        charges += 10;
+        if(pizzasOrdered[i].pSize === "md"){charges += 5}
+        else if(pizzasOrdered[i].pSize === "lg"){charges += 10}
+        charges += pizzasOrdered[i].toppingsCharge(i);
+    }
+    return charges;
+}
+
+var hawaiian
